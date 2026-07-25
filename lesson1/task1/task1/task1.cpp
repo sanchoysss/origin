5b492ebb6fa395ab83114c67dbc1068f2dfcec5d
test <sanchoys576@gmail.com>
2026-07-25 13:43:38 +0300

    Добавлена 1 задача

---
+++ lesson1/task1/task1/task1.cpp
@@ -0,0 +1,80 @@
+﻿
+#include <iostream>
+enum class Month
+{
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+};
+int main()
+{
+    std::setlocale(LC_ALL, "Russian");
+
+    int input = 0;
+    do {
+        std::cout << "Введите номер месяца: ";
+        std::cin >> input;
+
+        if (input == 0) {
+            std::cout << "До свидания" << std::endl;
+            break;
+        }
+
+        Month month = static_cast<Month>(input);
+        
+        switch (month) {
+        case Month::January:
+            std::cout << "Январь" << std::endl;
+            break;
+        case Month::February:
+            std::cout << "Февраль" << std::endl;
+            break;
+        case Month::March:
+            std::cout << "Март" << std::endl;
+            break;
+        case Month::April:
+            std::cout << "Апрель" << std::endl;
+            break;
+        case Month::May:
+            std::cout << "Май" << std::endl;
+            break;
+        case Month::June:
+            std::cout << "Июнь" << std::endl;
+            break;
+        case Month::July:
+            std::cout << "Июль" << std::endl;
+            break;
+        case Month::August:
+            std::cout << "Август" << std::endl;
+            break;
+        case Month::September:
+            std::cout << "Сентябрь" << std::endl;
+            break;
+        case Month::October:
+            std::cout << "Октябрь" << std::endl;
+            break;
+        case Month::November:
+            std::cout << "Ноябрь" << std::endl;
+            break;
+        case Month::December:
+            std::cout << "Декабрь" << std::endl;
+            break;
+        default:
+            std::cout << "Неправильный номер!" << std::endl;
+            break;
+        }
+
+
+    } while (true);
+    
+}
+
