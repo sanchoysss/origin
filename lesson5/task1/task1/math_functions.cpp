cb960431cf7389ca339b8d13c553ea6a8447260a
test <sanchoys576@gmail.com>
2026-08-09 12:43:09 +0300

    Добавлено 1 задание

---
+++ lesson5/task1/task1/math_functions.cpp
@@ -0,0 +1,12 @@
+#include "math_functions.h"
+#include <cmath>
+
+double add(double num1, double num2) { return num1 + num2; }
+
+double subtract(double num1, double num2) { return num1 - num2; }
+
+double multiply(double num1, double num2) { return num1 * num2; }
+
+double divide(double num1, double num2) { return num1 / num2; }
+
+double power(double num1, double num2) { return std::pow(num1,num2); }
