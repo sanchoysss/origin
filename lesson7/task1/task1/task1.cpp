4b7f5bf750846b8beff3f01d35c7f844c325949e
test <sanchoys576@gmail.com>
2026-08-16 20:50:59 +0300

    добавлен 1 номер

---
+++ lesson7/task1/task1/task1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+#include <exception>
+
+
+class bad_length : public std::exception {
+public:
+	const char* what() const noexcept override {
+		return "Вы ввели слово запретной длины! До свидания";
+	}
+};
+
+int function(std::string str, int forbidden_length) {
+	int current_length = static_cast<int>(str.length());
+
+	if (current_length == forbidden_length) {
+		throw bad_length();
+	}
+	return current_length;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int forbidden_length = 0;
+	std::cout << "Введите запретную длину: ";
+	std::cin >> forbidden_length;
+	std::cin.ignore();
+
+	while (true) {
+		std::string user_word;
+		std::cout << "Введите слово: ";
+		std::getline(std::cin, user_word);
+		
+
+		try {
+			int length = function(user_word, forbidden_length);
+			std::cout << "Длина слова \"" << user_word << "\" равна " << length << std::endl;
+		}
+
+		catch (const bad_length& e) {
+			std::cout << e.what() << std::endl;
+			break;
+		}
+	}
+	return 0;
+}
