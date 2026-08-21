7635324d8d688d7eda431ba0ca933579a1dfa75c
test <sanchoys576@gmail.com>
2026-08-21 19:44:37 +0300

    добавлен 1 номер

---
+++ lesson9/my_first_cmake_project/my_first_cmake_project.cpp
@@ -0,0 +1,16 @@
+﻿#include "my_first_cmake_project.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    cout << "Введите имя: ";
+    string name;
+    getline(cin, name);
+    cout << "Здравствуйте, " << name << "!" << endl;
+
+    return 0;
+}
