3fabb41f36b9ddf79acff790c321121a1ef4cc97
test <sanchoys576@gmail.com>
2026-08-30 11:36:01 +0300

    переделан 3 номер

--- lesson10/task3/LibraryDynamic.h
+++ lesson10/task3/LibraryDynamic.h
@@ -3,15 +3,9 @@
 #ifndef LIBRARYDYNAMIC_H
 #define LIBRARYDYNAMIC_H
 
-#ifdef LIBRARYDYNAMIC_EXPORTS
-#define TASKLIBRARY_API __declspec(dllexport)
-#else
-#define TASKLIBRARY_API __declspec(dllimport)
-#endif 
-
 
 namespace LibraryDynamic {
-	class TASKLIBRARY_API Leaver {
+	class Leaver {
 	public:
 		std::string leave(const std::string& name);
 	};
