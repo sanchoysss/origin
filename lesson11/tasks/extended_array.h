a4c27d8957b720a708e3c87ac567616599182558
test <sanchoys576@gmail.com>
2026-08-31 19:41:33 +0300

    добавлено задание

---
+++ lesson11/tasks/extended_array.h
@@ -0,0 +1,126 @@
+#pragma once
+#include <utility>
+#include <initializer_list>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+#include <stdexcept>
+#include <typeinfo>
+
+#include <iterator>
+
+template <typename T>
+class ExtArray
+{
+private:
+	std::vector<T> extended_array;
+	size_t _size;
+public:
+	ExtArray(std::initializer_list<T> l) : extended_array(l)
+	{
+		_size = l.size();
+	}
+
+	ExtArray(int size) : _size(size)
+	{
+		extended_array.resize(_size);
+	}
+
+	T& operator[](size_t index)
+	{
+		return extended_array[index];
+	}
+
+	size_t size()
+	{
+		return _size;
+	}
+
+	double mean()
+	{
+		if (_size == 0) { return 0; }
+		double sum = 0;
+		for (size_t i = 0; i < _size; i++)
+		{
+			sum += extended_array[i];
+		}
+		return sum / _size;
+	}
+
+	double median()
+	{
+		if (_size == 0) { return 0; }
+
+		std::vector<T> temp_array;
+		std::copy(extended_array.begin(), extended_array.end(), back_inserter(temp_array));
+		std::sort(temp_array.begin(), temp_array.end());
+		if (_size % 2 == 1)
+		{
+			return temp_array[_size / 2];
+		}
+		else
+		{
+			return static_cast<double>(temp_array[(_size / 2) - 1] + temp_array[_size / 2]) / 2;
+		}
+	}
+
+	std::pair<T, int> mode()
+	{
+		if (_size == 0) { return { T(), 0 }; }
+
+		T max = extended_array[0], cmax = 0, rmax = 0;
+		for (int i = 0; i < _size; i++)
+		{
+			if (cmax > rmax)
+			{
+				rmax = cmax;
+				max = extended_array[i - 1];
+			}
+			cmax = 0;
+			for (int j = i; j < _size; j++)
+			{
+				if (extended_array[j] == extended_array[i])
+				{
+					cmax++;
+				}
+			}
+		
+		
+		}
+		return std::pair<T, int>(max, rmax);
+	}
+
+
+	double mean(size_t start, size_t end) {
+		if (start >= end || end > _size) {
+			throw std::invalid_argument("Неверный диапазон");
+		}
+
+		double sum = 0;
+		for (size_t i = start; i < end; i++) {
+			sum += extended_array[i];
+		}
+		return sum / (end - start);
+	}
+
+
+	int checkSum(){
+		if (typeid(T) != typeid(int) && typeid(T) != typeid(bool)) {
+			throw std::bad_typeid();
+		}
+		int count = 0;
+		for (size_t i = 0; i < _size; i++) {
+			if (typeid(T) == typeid(int)) {
+				if (extended_array[i] != 0 && extended_array[i] != 1) {
+					throw std::logic_error("Не подходящие значения");
+				}
+			}
+			if (extended_array[i] == 1) {
+					count++;
+				}
+		}
+		return count;
+	}
+};
+
---
+++ lesson11/tasks/tasks.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <cassert>
+#include <stdexcept>
+#include <typeinfo>
+#include "extended_array.h"
+
+void test1() {
+    ExtArray<int> empty_arr{};
+    assert(empty_arr.mean() == 0);
+    assert(empty_arr.median() == 0);
+    assert(empty_arr.mode().second == 0);
+    std::cout << "Test 1 OK" << std::endl;
+}
+void test2() {
+    ExtArray<int> arr{ 1, 2, 3, 4, 5 };
+    assert(arr.mean(1, 4) == 3.0);
+
+    bool exception_caught = false;
+    try {
+        arr.mean(4, 2);
+    }
+    catch (const std::invalid_argument&) {
+        exception_caught = true;
+    }
+    assert(exception_caught);
+    std::cout << "Test 2 OK" << std::endl;
+}
+void test3() {
+    ExtArray<int> valid_int_bits{ 1, 0, 1, 1, 0 };
+    assert(valid_int_bits.checkSum() == 3);
+
+    ExtArray<bool> valid_bool_bits{ true, false, true };
+    assert(valid_bool_bits.checkSum() == 2);
+
+    bool bad_type_caught = false;
+    try {
+        ExtArray<double> double_arr{ 1.0, 0.0 };
+        double_arr.checkSum();
+    }
+    catch (const std::bad_typeid&) {
+        bad_type_caught = true;
+    }
+    assert(bad_type_caught);
+
+
+    bool logic_error_caught = false;
+    try {
+        ExtArray<int> invalid_int_bits{ 0, 1, 5, 0 };
+        invalid_int_bits.checkSum();
+    }
+    catch (const std::logic_error&) {
+        logic_error_caught = true;
+    }
+    assert(logic_error_caught);
+
+    std::cout << "Test 3 OK" << std::endl;
+}
+
+int main()
+{
+    try {
+        test1();
+        test2();
+        test3();
+
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
