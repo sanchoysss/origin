025ba1d006795d14c599bcf8753a8e85ffc9e364
test <sanchoys576@gmail.com>
2026-09-20 12:20:37 +0300

    добавлено 1 задание

---
+++ lesson14/task1/task1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+long long func(int n) {
+    if (n <= 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+
+    return func(n - 1) + func(n - 2);
+}
+
+int main() {
+    int n;
+    std::cout << "Введите номер числа Фибоначчи (n): ";
+    std::cin >> n;
+
+    std::cout << "Результат: " << func(n) << std::endl;
+
+    return 0;
+}
+// по времени o(2^n) из-за разветвления каждого вызова на два новых
+// по памяти o(n) т.к. максимальная грубина стека вызовов пропорциональна n
