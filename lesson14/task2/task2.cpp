f34337b3d7fa1cc0842c787d0409772a4998b071
test <sanchoys576@gmail.com>
2026-09-20 12:47:10 +0300

    добавлено 2 звдвние

---
+++ lesson14/task2/task2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+std::vector <long long> new_arr;
+
+long long fibonachi(int n) {
+    if (n <= 0) { return 0; }
+    if (n == 1) { return 1; }
+
+    if (new_arr[n] != 0) {
+        return new_arr[n];
+    }
+
+    new_arr[n] = fibonachi(n - 1) + fibonachi(n - 2);
+    return new_arr[n];
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int n;
+    std::cout << "Введите номер числа Фибоначчи (n): ";
+
+    std::cin >> n;
+
+    if (n < 0) {
+        std::cout << "Номер не может быть отрицательным" << std::endl;
+        return 1;
+    }
+
+    new_arr.resize(n + 1, 0);
+
+    std::cout << "Результат: " << fibonachi(n) << std::endl;
+}
